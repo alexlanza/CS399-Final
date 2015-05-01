c71fc7d7c814f8ae92ab9e287826eae8b47075d9
kyleamcginn88 <kyle.a.mcginn@gmail.com>
2015-04-30 18:37:06 -0700

    Added course section and passing in template values

--- PeerEval/engineapp/handlers/dashboard.py
+++ PeerEval/engineapp/handlers/dashboard.py
@@ -5,24 +5,35 @@ import webapp2
 import jinja2
 import os
 from handlers import BaseHandler
+from models import CourseSection
 
 class Dashboard(BaseHandler):
 
-	def get(self):
-		if self.user:
-			if self.user_profile:
-				#User has profile
-				if self.user_profile[0].role == 'student':
-					self.render("dashboard_student.html", self.template_values)
-				elif self.user_profile[0].role == 'instructor':
-					self.render("dashboard_instructor.html", self.template_values)
-			else:
-				#User does not have profile
-				self.redirect("/landing")
-		else:
-			#pass into template, this should be the destination for the button
-			self.url = users.create_login_url(self.request.uri)
-			self.url_linktext = 'Login'
+    def get(self):
+        tempValues = self.template_values
 
-	def post(self):
-		self.response.write('Submitted')
+        if self.user:
+            if self.user_profile:
+                #User has profile
+                if self.user_profile[0].role == 'student':
+                    self.render("dashboard_student.html", self.template_values)
+                elif self.user_profile[0].role == 'instructor':
+                    qry = CourseSection.query()
+                    qry = qry.filter(CourseSection.instructor==self.user)
+                    courses = qry.fetch()
+                    #tempValues = {}
+
+
+                    tempValues.update(self.template_values)
+
+                    self.render("dashboard_instructor.html", tempValues)
+            else:
+                #User does not have profile
+                self.redirect("/landing")
+        else:
+            #pass into template, this should be the destination for the button
+            self.url = users.create_login_url(self.request.uri)
+            self.url_linktext = 'Login'
+
+    def post(self):
+        self.response.write('Submitted')
