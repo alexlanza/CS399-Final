9f037057de1231473131daa8877f14075d4bc58b
aa2468 <aa2468@nau.edu>
2015-04-30 19:29:28 -0700

    add course section roster form and model

--- PeerEval/engineapp/models/course_section_roster.py
+++ PeerEval/engineapp/models/course_section_roster.py
@@ -2,4 +2,4 @@ from google.appengine.ext import ndb
 from models import CourseSection
 class CourseSectionRoster(ndb.Model):
 	course_section = ndb.KeyProperty(CourseSection) #Foreign Key Course Section
-	user = ndb.UserProperty(repeated=True) #Foreign Key to User
+	email = ndb.TextProperty()
